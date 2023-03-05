893e7bee2e37c16891a8ad3643b321acb1aad480
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-03-05 09:50:18 +0000

    sub lab06 v1

--- js/app.js
+++ js/app.js
@@ -18,3 +18,34 @@ else(alert(userName))
 }
 else(console.log("did not want the welcoming message"))
 
+alert ('please answer the following questions in "yes" or "no" ')
+
+const userInput = (userInput) => {   
+let developer = prompt("are you a developer ?")
+let knowJavaScript = prompt("do you know java script ?")
+let useful = prompt("is this website useful for you ? ") 
+
+let userInputArray = [ developer , knowJavaScript , useful ]
+return userInputArray
+}
+
+let inputNumber1 = userInput()
+
+
+
+
+const checkFunction = (arr) => {
+    
+    for(let i = 0 ; i<arr.length ; i++){
+        if(arr[i] == null || arr[i] == undefined || arr[i] == "" ){ 
+            arr[i] = "invalid"
+        }
+    }
+    return arr
+}
+
+
+let inputNumber1Checked = checkFunction(inputNumber1)
+
+console.log(inputNumber1Checked)
+
